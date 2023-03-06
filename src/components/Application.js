ce29d35ad66d16f436ca1592db1b997ad2fee07b
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-03-05 19:08:32 -0500

    Add useEffect to Request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+import axios from "axios";
 
 const appointments = {
   "1": {
@@ -63,6 +46,8 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
   const appointmentArray = Object.values(appointments).map(appointment => {
     return (
       <Appointment 
@@ -71,6 +56,14 @@ export default function Application(props) {
       />
     )
   });
+
+  useEffect(() => {
+    const daysURL = `http://localhost:8001/api/days`;
+    axios.get(daysURL).then(response => {
+      setDays([...response.data])
+    });
+  }, []);
+  
   return (
     <main className="layout">
       <section className="sidebar">
